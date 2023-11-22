c572f80e0f2378c5b42fffba9508299d127982e8
Paul Michael <paulebi4eva@gmail.com>
2023-11-22 23:03:53 +0100

    Snubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,8 @@
-'use strict';
-const Utils = require('./utils.js');
+const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${res}`);
-};
+    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
+  
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,24 @@
-'use strict';
-const chai = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
 
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi function', () => {
-  const spyConsole = sinon.spy(console, 'log');
-
-  it('validate the usage of the Utils function', () => {
-    const stubUtils = sinon.stub(Utils, 'calculateNumber');
-    stubUtils.withArgs('SUM', 100, 20).returns(10);
+describe('sendPaymentRequestToApi', () => {
+  const consoleSpy = sinon.spy(console, 'log');
+  it('validates usage of Utils.calculateNumber', () => {
+    // Stub the function always return the same number 10
+    const calcNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-    chai.expect(spyConsole.calledOnce).to.be.true;
-    chai.expect(spyConsole.calledWith('The total is: 10')).to.be.true;
-    stubUtils.restore()
-    spyConsole.restore();
+    // check that the stub is being called with type = SUM, a = 100, and b = 20
+    expect(calcNumStub.calledWith('SUM', 100, 20)).to.be.true;
+    // check that the stub always return the same number 10
+    expect(calcNumStub.alwaysReturned(10)).to.be.true;
+    // check with spy that console.log is logging the correct message 
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+
+    calcNumStub.restore();
+    consoleSpy.restore();
   });
 });
